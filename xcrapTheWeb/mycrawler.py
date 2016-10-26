6d104a5161a51ddbcce906a089b2300c69438fd6
Sankalp Yadav <say2sankalp90@gmail.com>
2016-10-26 13:08:20 +0530

    readability of code.

--- xcrapTheWeb/mycrawler.py
+++ xcrapTheWeb/mycrawler.py
@@ -4,18 +4,21 @@ import  datetime
 import random
 import re
 
-pages=set()
+
 def get_links(url):
     global pages
     html = urlopen("http://en.wikipedia.org"+url)
     bsobj= BeautifulSoup(html.read())
-    return  bsobj.find("div",{"id":"bodyContent"})\
+    link_pages=  bsobj.find("div",{"id":"bodyContent"})\
         .findAll("a",href=re.compile("^(/wiki/)((?!:).)*$"))
+    return link_pages
 
 
 links = get_links("/wiki/Attention_deficit_hyperactivity_disorder")
+print type(links)
+
 
 while len(links) >0:
     newarticle= links[random.randint(0,len(links)-1)].attrs['href']
     print newarticle
-    links= get_links(newarticle)
+    links= list(set(get_links(newarticle)))
